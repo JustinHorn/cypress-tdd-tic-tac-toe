6514355a2a94a45cb53e28a85d9a0aa7bcdce527
Justin Horn <justinhorn0000@gmail.com>
2021-12-20 10:47:06 +0100

    have all basic features for TTT

--- cypress/integration/app.spec.js
+++ cypress/integration/app.spec.js
@@ -5,17 +5,17 @@ describe("App", () => {
     cy.visit("http://localhost:3000");
   });
 
-  it("Has a title", () => {
-    cy.contains("Hello World!");
-  });
+  // it("Has a title", () => {
+  //   cy.contains("Hello World!");
+  // });
 
-  it("Has a button", () => {
-    cy.on("window:alert", (txt) => {
-      //Mocha assertions
-      expect(txt).to.contains("Hello World!");
-    });
-    cy.contains("Hello World Alert").click();
-  });
+  // it("Has a button", () => {
+  //   cy.on("window:alert", (txt) => {
+  //     //Mocha assertions
+  //     expect(txt).to.contains("Hello World!");
+  //   });
+  //   cy.contains("Hello World Alert").click();
+  // });
 
   describe("field", () => {
     it("should have 9 tiles", () => {
@@ -31,10 +31,15 @@ describe("App", () => {
         cy.get("#field .tile:first").click().contains("X");
         cy.get("#field .tile:nth-child(2)").click().contains("O");
       });
+
+      it("click same field twice", () => {
+        cy.get("#field .tile:nth-child(1)").click().contains("X");
+        cy.get("#field .tile:nth-child(1)").click().contains("X");
+      });
     });
   });
 
-  describe("game-ends", () => {
+  describe("game-over", () => {
     it("x-wins", () => {
       cy.get("#field .tile:nth-child(1)").click();
       cy.get("#field .tile:nth-child(4)").click();
@@ -42,6 +47,71 @@ describe("App", () => {
       cy.get("#field .tile:nth-child(5)").click();
       cy.get("#field .tile:nth-child(3)").click();
       cy.contains("Player X has won!");
+    });
+
+    it("O-wins", () => {
+      cy.get("#field .tile:nth-child(1)").click();
+      cy.get("#field .tile:nth-child(4)").click();
+      cy.get("#field .tile:nth-child(2)").click();
+      cy.get("#field .tile:nth-child(5)").click();
+      cy.get("#field .tile:nth-child(7)").click();
+      cy.get("#field .tile:nth-child(6)").click();
+      cy.contains("Player O has won!");
+    });
+
+    it("X-wins-\\-diagonal", () => {
+      cy.get("#field .tile:nth-child(1)").click();
+      cy.get("#field .tile:nth-child(4)").click();
+      cy.get("#field .tile:nth-child(5)").click();
+      cy.get("#field .tile:nth-child(7)").click();
+      cy.get("#field .tile:nth-child(9)").click();
+      cy.contains("Player X has won!");
+    });
+
+    it("O-wins-/-diagonal", () => {
+      cy.get("#field .tile:nth-child(1)").click();
+      cy.get("#field .tile:nth-child(7)").click();
+      cy.get("#field .tile:nth-child(2)").click();
+      cy.get("#field .tile:nth-child(5)").click();
+      cy.get("#field .tile:nth-child(4)").click();
+      cy.get("#field .tile:nth-child(3)").click();
+      cy.contains("Player O has won!");
+    });
+    it("draw", () => {
+      cy.get("#field .tile:nth-child(1)").click();
+      cy.get("#field .tile:nth-child(5)").click();
+      cy.get("#field .tile:nth-child(2)").click();
+      cy.get("#field .tile:nth-child(3)").click();
+      cy.get("#field .tile:nth-child(7)").click();
+      cy.get("#field .tile:nth-child(4)").click();
+      cy.get("#field .tile:nth-child(6)").click();
+      cy.get("#field .tile:nth-child(8)").click();
+      cy.get("#field .tile:nth-child(9)").click();
+      cy.contains("Player X has won!").should("not.exist");
+      cy.contains("Player O has won!").should("not.exist");
+      cy.contains("Game Over: DRAW!");
+    });
+
+    it("cant click after game over", () => {
+      //
+      cy.get("#field .tile:nth-child(1)").click();
+      cy.get("#field .tile:nth-child(4)").click();
+      cy.get("#field .tile:nth-child(2)").click();
+      cy.get("#field .tile:nth-child(5)").click();
+      cy.get("#field .tile:nth-child(3)").click();
+      cy.contains("Player X has won!");
+
+      cy.get("#field .tile:nth-child(7)").click();
+      cy.get("#field .tile:nth-child(7)").contains("O").should("not.exist");
+    });
+  });
+
+  describe("features", () => {
+    it("restart the game", () => {
+      cy.get("button").contains("restart");
+      cy.get("#field .tile:first").click().contains("X");
+      cy.get("button").contains("restart").click();
+      cy.get("#field .tile:first").contains("X").should("not.exist");
     });
   });
 });
--- src/App.js
+++ src/App.js
@@ -1,48 +1,61 @@
 import { useState, useMemo } from "react";
 import "./App.css";
 
+const EMPTY = " ";
+const X = "X";
+const O = "O";
+const EMPTY_FIELD = Array(9).fill(EMPTY);
+
 function App() {
-  const [field, setField] = useState(Array(9).fill(" "));
+  const [field, setField] = useState(EMPTY_FIELD);
 
   const isXTurn = useMemo(
-    () => field.filter((x) => x === " ").length % 2,
+    () => field.filter((x) => x === EMPTY).length % 2,
     [field]
   );
 
   const gameStatus = useMemo(() => {
     //
 
-    const nonEmptyEqual = (a, b, c) => a !== " " && a === b && b === c;
+    const nonEmptyEqual = (a, b, c) => a !== EMPTY && a === b && b === c;
     for (let i = 0; i < 3; i++) {
       // horizontal & vertical
-      if (nonEmptyEqual(field[i], field[i + 1], field[i + 2])) return field[i];
+      if (nonEmptyEqual(field[i * 3], field[i * 3 + 1], field[i * 3 + 2]))
+        return field[i * 3];
       if (nonEmptyEqual(field[i], field[i + 3], field[i + 6])) return field[i];
     }
+    if (nonEmptyEqual(field[0], field[4], field[8])) return field[4];
+    if (nonEmptyEqual(field[6], field[4], field[2])) return field[4];
+
+    return field.some((f) => f === EMPTY) ? "NOT_OVER" : "DRAW";
   }, [field]);
 
   return (
     <div className="App">
-      <h1>Hello World!</h1>
-      <button onClick={() => alert("Hello World!")}>Hello World Alert</button>
-
+      {gameStatus === X && <h1>Player X has won!</h1>}
+      {gameStatus === O && <h1>Player O has won!</h1>}
+      {gameStatus === "DRAW" && <h1>Game Over: DRAW!</h1>}
+      <button onClick={() => setField(EMPTY_FIELD)}>restart</button>
       <div id="field">
         {field.map((x, index) => (
           <div
             onClick={() =>
-              setField((prev) => [
-                ...prev.slice(0, index),
-                isXTurn ? "X" : "O",
-                ...prev.slice(index + 1),
-              ])
+              setField((prev) =>
+                prev[index] === EMPTY && gameStatus === "NOT_OVER"
+                  ? [
+                      ...prev.slice(0, index),
+                      isXTurn ? X : O,
+                      ...prev.slice(index + 1),
+                    ]
+                  : prev
+              )
             }
-            className={`tile`}
+            className="tile"
           >
             {x}
           </div>
         ))}
       </div>
-
-      {gameStatus === "X" && <h1>Player X has won</h1>}
     </div>
   );
 }
